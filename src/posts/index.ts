4bfc5415a2d3e3ac47bab18d3128cba09be1e3bc
Philipp A <flying-sheep@web.de>
2018-10-31 19:09:12 +0100

    moved non-component Post class to posts

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,5 +1,28 @@
 import rawPosts from './markup'
-import Post from '../components/Blog/Post'
+import { Markdown, ReStructuredText } from '../components/markup'
+
+export class Post {
+	title: string
+	renderer: Markdown | ReStructuredText
+	date: Date
+	markup: string
+	format: 'md' | 'rst'
+	element: React.ReactElement<any>
+	
+	constructor(date: Date, markup: string, format: 'md' | 'rst') {
+		this.date = date
+		this.markup = markup
+		this.format = format
+		const Renderer = { md: Markdown, rst: ReStructuredText }[format]
+		this.renderer = new Renderer(markup)
+		try {
+			this.title = this.renderer.getTitle()
+		} catch (e) {
+			this.title = e.toString()
+		}
+		this.element = this.renderer.render()
+	}
+}
 
 const posts = Object.entries(rawPosts)
 	.reduce((obj, [filename, markup]) => {
