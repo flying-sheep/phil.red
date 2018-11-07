d840c42aa8b7a33fbaa375f5824733c6eeda3637
Philipp A <flying-sheep@web.de>
2018-11-07 10:10:34 +0100

    fixed eslint

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -21,6 +21,7 @@ export class Post {
 const posts = Object.entries(rawPosts)
 	.reduce((obj, [filename, markup]) => {
 		const [, y, m, d, slug, format] = /(\d{4})-(\d{2})-(\d{2})-(.+)\.(rst|md)/.exec(filename) as RegExpExecArray
+		// eslint-disable-next-line no-param-reassign
 		obj[slug] = new Post(
 			new Date(+y, +m - 1, +d),
 			markup,
