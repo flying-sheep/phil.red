aa32f638d6e371dc88092f9d8265a3bc3869ad48
Philipp A <flying-sheep@web.de>
2020-03-10 01:42:33 +0100

    Improve list

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -32,9 +32,12 @@ const ListItemLink = ({
 
 const GitHubList = () => (
 	<List>
-		<ListItemLink href="https://github.com/theislab/scanpy" icon={i.AllInclusive} sub="single cell data analysis">scanpy</ListItemLink>
-		<ListItemLink href="https://github.com/theislab/anndata" icon={i.ViewComfy} sub="annotated single cell expression matrix">AnnData</ListItemLink>
-		<ListItemLink href="https://github.com/theislab/anndata2ri" icon={i.SwapCalls} sub="convert between AnnData and SingleCellExperiment">anndata2ri</ListItemLink>
+		<ListItem><ListItemText primary="2nd author"/></ListItem>
+		<ListItemLink href="https://github.com/theislab/scanpy" icon={i.AllInclusive} sub="single cell analysis in Python">scanpy</ListItemLink>
+		<ListItem><ListItemText primary="1st, main, or only author"/></ListItem>
+		<ListItemLink href="https://github.com/theislab/anndata" icon={i.ViewComfy} sub="annotated single cell expression matrix for Python">AnnData</ListItemLink>
+		<ListItemLink href="https://github.com/theislab/anndata2ri" icon={i.SwapCalls} sub="convert between AnnData (Python) and SingleCellExperiment (R)">anndata2ri</ListItemLink>
+		<ListItemLink href="https://github.com/theislab/destiny" icon={i.BlurOn} sub="diffusion maps, pseudotime, and gene relevance in R">destiny</ListItemLink>
 		<ListItemLink href="https://github.com/IRkernel/IRkernel" icon={i.BubbleChart} sub="R kernel for JupyterLab/Notebooks">IRkernel</ListItemLink>
 		<ListItemLink href="https://github.com/IRkernel/repr" icon={i.EmojiSymbols} sub="rich representations for R objects">repr</ListItemLink>
 		<ListItemLink href="https://github.com/flying-sheep/rust-rst" icon={i.Settings} sub="reStructuredText parser and renderer in Rust">rust-rst</ListItemLink>
@@ -48,7 +51,6 @@ export default function Blog({ match }: RouteComponentProps) {
 			<ListItemLink href="https://github.com/flying-sheep" icon={i.GitHub} sub="flying-sheep">GitHub</ListItemLink>
 			<ListItemLink href="https://pypi.org/user/flyingsheep/" icon={PythonIcon} sub="My Python packages (me=author)">PyPI</ListItemLink>
 			<ListItemLink href="https://aur.archlinux.org/packages/?K=flying-sheep&amp;SeB=m" icon={ArchIcon} sub="My Arch Linux packages (me=packager)">AUR</ListItemLink>
-			<ListItem><ListItemText primary="I substantially or exclusively made the following:"/></ListItem>
 			<GitHubList/>
 		</>
 	)
