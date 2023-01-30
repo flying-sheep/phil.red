1aa0fc9e66a28627e19f81a0c656d8469d4f6a89
Philipp A <flying-sheep@web.de>
2023-01-30 16:52:34 -0500

    Fix indentation

--- typings/restructured/index.d.ts
+++ typings/restructured/index.d.ts
@@ -1,21 +1,21 @@
 declare module 'restructured' {
-    export type InlineNodeType = (
-        'text' |
+	export type InlineNodeType = (
+		'text' |
 		'literal' |
 		'emphasis' |
 		'strong'
 	)
-    export type BlockNodeType = (
-        'document' |
-        'section' |
-        'comment' |
-        'reference' |
-        'title' |
+	export type BlockNodeType = (
+		'document' |
+		'section' |
+		'comment' |
+		'reference' |
+		'title' |
 		'paragraph' |
 		'block_quote' |
 		'interpreted_text' |
 		'literal_block' |
-        'directive' |
+		'directive' |
 		'bullet_list' |
 		'enumerated_list' |
 		'list_item' |
@@ -39,25 +39,25 @@ declare module 'restructured' {
 	export interface NodeWithPos {
 		position: { start: Positon, end: Positon },
 	}
-    export type BlockNode<HasPos> = BaseNode<HasPos> & {
-        type: BlockNodeType
+	export type BlockNode<HasPos> = BaseNode<HasPos> & {
+		type: BlockNodeType
 		children: Node<HasPos>[]
 		blanklines?: string[]
 		indent?: { width: number, offset: number }
 	}
-    export type InlineNode<HasPos> = BaseNode<HasPos> & {
-        type: InlineNodeType
-        value: string
+	export type InlineNode<HasPos> = BaseNode<HasPos> & {
+		type: InlineNodeType
+		value: string
 	}
 	export type Section<HasPos> = BlockNode<HasPos> & { type: 'section'; depth: number }
 	export type BulletList<HasPos> = BlockNode<HasPos> & { type: 'bullet_list'; bullet: BulletType }
 	export type Directive<HasPos> = BlockNode<HasPos> & { type: 'directive'; directive: DirectiveType }
 	export type InterpretedText<HasPos> = BlockNode<HasPos> & { type: 'interpreted_text'; role: RoleType }
-    export type OtherBlockNode<HasPos> = BlockNode<HasPos> & {
-        type: Exclude<BlockNodeType, 'section' | 'bullet_list' | 'directive' | 'interpreted_text'>
+	export type OtherBlockNode<HasPos> = BlockNode<HasPos> & {
+		type: Exclude<BlockNodeType, 'section' | 'bullet_list' | 'directive' | 'interpreted_text'>
 	}
-    export type OtherInlineNode<HasPos> = InlineNode<HasPos> & {
-        // type: Exclude<InlineNodeType, ...>
+	export type OtherInlineNode<HasPos> = InlineNode<HasPos> & {
+		// type: Exclude<InlineNodeType, ...>
 	}
 	export type Node<HasPos> = (
 		Section<HasPos> | BulletList<HasPos> | Directive<HasPos> | InterpretedText<HasPos> |
@@ -68,12 +68,12 @@ declare module 'restructured' {
 		blanklines?: boolean
 		indent?: boolean
 	} & (HasPos extends true ? { position: true } : { position?: false })
-    interface RST {
-        parse(code: string, options?: Options<true>): Node<true>
-        parse(code: string, options?: Options<false>): Node<false>
-    }
+	interface RST {
+		parse(code: string, options?: Options<true>): Node<true>
+		parse(code: string, options?: Options<false>): Node<false>
+	}
 	const weird: { default: RST }
-    export default weird
+	export default weird
 }
 
 declare module 'restructured/lib/Parser.js' {
