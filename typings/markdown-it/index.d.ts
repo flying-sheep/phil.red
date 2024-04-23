56037c547377e8ea45cf01e188a4d886808ea0bb
Philipp A. <flying-sheep@web.de>
2024-04-23 09:45:12 +0200

    remove hack

--- typings/markdown-it/index.d.ts
+++
@@ -1,6 +0,0 @@
-// Hack waiting for 14.0 release of https://www.npmjs.com/package/@types/markdown-it
-declare module 'markdown-it/lib/token.mjs' {
-	import Token from 'markdown-it/lib/token.js'
-
-	export = Token
-}
