82c96473989db9168e7496f19dc8a45c1c4be918
Philipp A. <flying-sheep@web.de>
2024-09-03 11:06:52 +0200

    Use vars everywhere

--- src/components/routes/App/index.tsx
+++ src/components/routes/App/index.tsx
@@ -85,8 +85,8 @@ const App = () => {
 				<AppBar
 					position="sticky"
 					sx={{
-						color: theme.palette.text.primary,
-						background: alpha(theme.palette.background.default, 0.7),
+						color: theme.vars.palette.text.primary,
+						background: `color(from ${theme.vars.palette.background.default} / 0.7)`,
 						// TODO re-add contrast(200%) before blur without discoloring dark mode
 						backdropFilter: 'blur(15px)',
 					}}
