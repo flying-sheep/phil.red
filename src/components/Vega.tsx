efe46c6f9f6ba4296e67c98dcf60e52460cfd854
Philipp A. <flying-sheep@web.de>
2025-08-22 18:29:22 +0200

    almost!

--- src/components/Vega.tsx
+++ src/components/Vega.tsx
@@ -1,4 +1,3 @@
-//import { useParentSize } from '@cutting/use-get-parent-size'
 import { useTheme } from '@mui/material/styles'
 import { type FC, useRef } from 'react'
 import type { Config } from 'vega-lite'
@@ -7,14 +6,13 @@ import { useVegaEmbed, type VegaEmbedProps } from './VegaEmbed'
 
 const VegaLite: FC<VegaEmbedProps> = ({
 	spec,
-	options = {},
+	options,
 	onEmbed,
 	onError,
 	...divProps
 }) => {
 	const theme = useTheme()
 	const ref = useRef<HTMLDivElement>(null)
-	//const { width } = useParentSize(ref)
 
 	const vltheme = (
 		theme.palette.mode === 'dark' ? themes.dark : themes.carbonwhite
@@ -23,14 +21,21 @@ const VegaLite: FC<VegaEmbedProps> = ({
 	if (typeof spec === 'string') {
 		throw new Error('VegaLite: spec must be an object')
 	}
-	//options.width = width - 100
+	spec.width = 'container' // TODO: this is actually correct, make the types know.
+	spec.autosize = { type: 'fit', contains: 'padding', resize: true }
 	spec.config = {
 		...spec.config,
 		...vltheme,
 		background: 'transparent',
 	} as Config
 	useVegaEmbed({ ref, spec, options, onEmbed, onError })
-	return <div ref={ref} {...divProps} />
+	return (
+		<figure
+			ref={ref}
+			{...divProps}
+			style={{ width: '100%', boxSizing: 'border-box' }}
+		/>
+	)
 }
 
 export default VegaLite
