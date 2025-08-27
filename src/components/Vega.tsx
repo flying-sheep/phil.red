e216ba4d7716353de7c10b0945c90543b806cac4
Philipp A. <flying-sheep@web.de>
2025-08-27 10:08:58 +0200

    Use upstream react-vega (#839)

--- src/components/Vega.tsx
+++ src/components/Vega.tsx
@@ -1,51 +1,21 @@
-import Box, { type BoxProps } from '@mui/material/Box'
-import type { SxProps } from '@mui/system'
-import { mergeSx } from 'merge-sx'
-import { type FC, useMemo, useRef } from 'react'
-import type { VisualizationSpec } from 'vega-embed'
-import { mergeConfig } from 'vega-util'
-import { useMuiVegaOptions } from './mui-vega'
-import { type UseVegaEmbedParams, useVegaEmbed } from './VegaEmbed'
+import { CircularProgress, Stack } from '@mui/material'
+import { lazy, Suspense } from 'react'
+import type { VegaProps } from './VegaInner'
 
-export interface VegaProps
-	extends Omit<UseVegaEmbedParams, 'ref'>,
-		Omit<BoxProps<'figure'>, 'onError'> {
-	sx?: SxProps
-}
+export type { VegaProps }
 
-const Vega: FC<VegaProps> = ({
-	spec: rawSpec,
-	options,
-	onEmbed,
-	onError,
-	sx,
-	...boxProps
-}) => {
-	const ref = useRef<HTMLDivElement>(null)
-	const vgtheme = useMuiVegaOptions()
-	// TODO: fix types, merge deep
-	const spec = useMemo<VisualizationSpec>(
-		() =>
-			({
-				...rawSpec,
-				...{
-					autosize: { type: 'fit', contains: 'padding', resize: true },
-					config: mergeConfig(rawSpec.config ?? {}, vgtheme, {
-						background: 'transparent',
-					}),
-				},
-			}) as VisualizationSpec,
-		[rawSpec, vgtheme],
-	)
-	useVegaEmbed({ ref, spec, options, onEmbed, onError })
-	return (
-		<Box
-			ref={ref}
-			component="figure"
-			{...boxProps}
-			sx={mergeSx({ margin: 0 }, sx)}
-		/>
-	)
-}
+const VegaInner = lazy(() => import('./VegaInner'))
+
+const Vega = (props: VegaProps) => (
+	<Suspense
+		fallback={
+			<Stack alignItems="center">
+				<CircularProgress />
+			</Stack>
+		}
+	>
+		<VegaInner {...props} />
+	</Suspense>
+)
 
 export default Vega
