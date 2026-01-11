74c9d0f00c1e94121cb86a1a69c39b98abf337cf
Phil Schaf <flying-sheep@web.de>
2026-01-11 23:29:59 +0100

    less diff

--- src/components/routes/Home/index.tsx
+++ src/components/routes/Home/index.tsx
@@ -2,51 +2,53 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import { useNavigate, useParams } from 'react-router'
 
-const Home = () => {
+const Home = () => (
+	// TODO: fix route
+	<Container maxWidth="sm">
+		<Typography
+			component="h1"
+			variant="h2"
+			align="center"
+			color="textPrimary"
+			gutterBottom
+		>
+			phil.red
+		</Typography>
+		<Typography
+			variant="h5"
+			align="center"
+			color="textSecondary"
+			paragraph
+			sx={{ marginBlock: 4 }}
+		>
+			programming, science, emancipation
+		</Typography>
+		<Typography variant="h6" align="center" paragraph>
+			What software won’t sell you out?
+			<br />
+			When do we get full automation and eternal life?
+			<br />
+			Why are people blaming immigrants, not capitalism?
+		</Typography>
+		<Typography variant="h6" align="center" paragraph>
+			I won’t be the one to fix those questions.
+			<br />
+			Currently most progress happens for the first.
+			<br />
+			But I will never stop living for all.
+		</Typography>
+	</Container>
+	// <CardMedia image={firstPost.image} title={post.imageTitle} />
+)
+
+const HomeDev = () => {
 	const params = useParams<'p'>()
 	const navigate = useNavigate()
 	if (params.p) {
 		navigate(decodeURIComponent(params.p), { replace: true })
 		return null
 	}
-	return (
-		// TODO: fix route
-		<Container maxWidth="sm">
-			<Typography
-				component="h1"
-				variant="h2"
-				align="center"
-				color="textPrimary"
-				gutterBottom
-			>
-				phil.red
-			</Typography>
-			<Typography
-				variant="h5"
-				align="center"
-				color="textSecondary"
-				paragraph
-				sx={{ marginBlock: 4 }}
-			>
-				programming, science, emancipation
-			</Typography>
-			<Typography variant="h6" align="center" paragraph>
-				What software won’t sell you out?
-				<br />
-				When do we get full automation and eternal life?
-				<br />
-				Why are people blaming immigrants, not capitalism?
-			</Typography>
-			<Typography variant="h6" align="center" paragraph>
-				I won’t be the one to fix those questions.
-				<br />
-				Currently most progress happens for the first.
-				<br />
-				But I will never stop living for all.
-			</Typography>
-		</Container>
-		// <CardMedia image={firstPost.image} title={post.imageTitle} />
-	)
+	return <Home />
 }
 
-export default Home
+export default import.meta.env.DEV ? HomeDev : Home
