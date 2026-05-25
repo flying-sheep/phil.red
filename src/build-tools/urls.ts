555ac7d8c58668a04f8d6dcfdcdc3216449e6be6
Phil Schaf <flying-sheep@web.de>
2026-05-25 07:38:00 -0400

    chore: switch depcheck for knip

--- src/build-tools/urls.ts
+++
@@ -1,3 +0,0 @@
-export const get = (pkg: string, ver: string, file: string) =>
-	`https://cdnjs.cloudflare.com/ajax/libs/${pkg}/${ver}/${file}`
-export const prism = (file: string) => get('prism', '1.30.0', file)
